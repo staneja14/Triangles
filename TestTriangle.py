21d4d365a4dfa6e63a388e736777c85ec7033f96
Shivani Taneja <shivani@Shivanis-MBP.fios-router.home>
2020-09-09 22:01:23 -0400

    Testing Triangles

---
+++ TestTriangle.py
@@ -0,0 +1,31 @@
+import unittest
+
+from HWAssignments.Triangles import Triangle
+
+
+class TestTriangle(unittest.TestCase):
+
+    def test_classifyTriangle(self):
+
+        result = Triangle.classifyTriangle(1, 1, 1)
+        self.assertEqual(result, "Equilateral")
+
+        result1 = Triangle.classifyTriangle(1, 1, 2)
+        self.assertEqual(result1, "Isosceles")
+
+        result2 = Triangle.classifyTriangle(1, 2, 3)
+        self.assertEqual(result2, "Scalene")
+
+        result3 = Triangle.classifyTriangle(0, 0, 0)
+        self.assertEqual(result3, "Invalid Input")
+
+        result5 = Triangle.classifyTriangle(3, 3, 3)
+        self.assertEqual(result5, "Equilateral")
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ Triangle.py
@@ -0,0 +1,40 @@
+"""
+@author: Shivani Taneja
+
+This Python file contains a class to determine whether a triangle is scalene
+, isosceles, right or equilateral.
+
+"""
+
+
+def classifyTriangle(a, b, c):
+    """
+
+    :param a: Length of the first side of triangle
+    :param b: Length of the second side of triangle
+    :param c: Length of third side of triangle
+    :return: A String:
+
+    'Equilateral' : If all three sides are equal
+    'Isosceles' : If exactly two sides are equal
+    'Scalene' : If no two sides are equal
+    'Right': If sum of the squares of any two sides is equal to the third side
+ """
+
+    a = int(a)
+    b = int(b)
+    c = int(c)
+
+    if a == 0 or b == 0 or c == 0:
+        result = "Invalid Input"
+    elif a == b == c:
+        result = "Equilateral"
+    elif a == b or a == c or b == c:
+        result = "Isosceles"
+    elif a != b and a != c and b != c:
+        result = "Scalene"
+    elif (a ** 2) + (b ** 2) == (c ** 2):
+        result = "Right"
+    else:
+        result = "Please enter the sides of triangle"
+    return result
